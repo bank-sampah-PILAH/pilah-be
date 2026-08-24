da3eacdef4a7cdcc4ff8eea425ce6bd0263a5772
Vincentius Filbert Amadeo <vincentius.filbert@ui.ac.id>
2026-08-24 13:38:34 +0700

    Update Android asset link certificates

--- api/tests.py
+++ api/tests.py
@@ -741,17 +741,10 @@ class APISpecTests(APITestCase):
                         "namespace": "android_app",
                         "package_name": "com.mobile.pilahapp",
                         "sha256_cert_fingerprints": [
-                            "70:EF:3E:65:35:DD:83:3C:5B:43:79:E9:23:13:84:8E:E9:82:30:4F:A8:C6:E6:5F:A1:E5:3A:8A:6F:D8:EB:FC"
-                        ],
-                    },
-                },
-                {
-                    "relation": ["delegate_permission/common.handle_all_urls"],
-                    "target": {
-                        "namespace": "android_app",
-                        "package_name": "com.mobile.pilahapp",
-                        "sha256_cert_fingerprints": [
-                            "73:B6:CC:52:38:29:88:E0:38:DA:47:1E:68:F9:86:6F:C8:C5:3E:8E:5D:1B:51:BD:FF:FE:78:ED:C6:C4:97:D6"
+                            "B5:1A:6B:E6:CC:8F:00:0A:1E:BD:82:B9:6E:EB:80:68:B2:14:4D:FA:25:B5:B6:8A:E5:09:7D:DC:88:0F:70:36",
+                            "73:B6:CC:52:38:29:88:E0:38:DA:47:1E:68:F9:86:6F:C8:C5:3E:8E:5D:1B:51:BD:FF:FE:78:ED:C6:C4:97:D6",
+                            "A4:3F:BA:76:7F:D2:CA:A6:A9:8F:4A:99:66:78:47:AD:51:FB:97:6B:EE:C1:08:09:AC:EA:A8:64:A6:7A:36:3B",
+                            "70:EF:3E:65:35:DD:83:3C:5B:43:79:E9:23:13:84:8E:E9:82:30:4F:A8:C6:E6:5F:A1:E5:3A:8A:6F:D8:EB:FC",
                         ],
                     },
                 },
